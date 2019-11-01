0d4917c457cf999642ba50900967772ea0416e17
Ankush <ankushrawat950@gmail.com>
2019-11-02 01:54:58 +0530

    adding flask

---
+++ app.py
@@ -0,0 +1,8 @@
+#importing libraries
+import flask
+app = flask.Flask(__name__, template_folder='templates')
+@app.route('/')
+def main():
+    return(flask.render_template('index.html'))
+if __name__ == '__main__':
+    app.run()
